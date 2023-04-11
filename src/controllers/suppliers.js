64c33f3ac22589e3d4db48037a8c813dc3be5b16
Ana Paula Souza dos Santos <anapaulasouza2095@gmail.com>
2023-04-10 21:15:48 -0300

    limpando cod

--- src/controllers/suppliers.js
+++ src/controllers/suppliers.js
@@ -4,17 +4,17 @@ require('dotenv').config();
 const getSuppliers = async (req, res) => {
     const { consumption } = req.query;
 
-    if (consumption <= 0) {
-        return res.status(400).json({ "mensagem": "O valor do consumo é inválido." })
-    }
-
     if (!consumption) {
         return res.status(400).json({ "mensagem": "Informe o valor do consumo." })
     }
 
+    if (consumption <= 0) {
+        return res.status(400).json({ "mensagem": "O valor do consumo é inválido." })
+    }
+
     try {
         const response = await knex('fornecedores').whereRaw(`limite_min_kwh > ${consumption}`);
-        console.log(response)
+
         return res.json(response);
     } catch (error) {
         return res.status(400).json(error.message);
--- src/dump/db.sql
+++ src/dump/db.sql
@@ -10,3 +10,10 @@ CREATE TABLE fornecedores (
   total_clientes INT NOT NULL,
   avaliacao_media_clienes INT NOT NULL
 );
+
+INSERT INTO fornecedores (nome, logo, uf, custo_kwh, limite_min_kwh, total_clientes, avaliacao_media_clienes)
+VALUES 
+('COELBA', 'https://investidorsardinha.r7.com/wp-content/uploads/2020/05/coelba.png', 'BA', 10, 500, 622, 5),
+('COSERN', 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0021/7328/brand.gif?itok=kDZow1bg', 'RN', 12, 600, 622, 5),
+('ETO', 'https://t3.ftcdn.net/jpg/05/11/49/72/360_F_511497246_vHjAETaHkR1cPU2wHoNg6Zv7YwMwTr0m.jpg', 'TO', 10, 600, 622, 5),
+('EPB', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlnxf7khz-99lIpUDNfcQRz-LIxukIFkyKG6E5B-PpNQovo4nUoZtnuWu3QvqgUalFsNs&usqp=CAU', 'PB', 10, 500, 622, 5);
